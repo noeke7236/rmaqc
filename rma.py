819041b25ff8087cd8afa6e3665f7f3df2970866
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2023-12-25 19:59:27 +0700

    Update rma.py

--- rma.py
+++ rma.py
@@ -1,8 +1,17 @@
 # Contents of ~/rmaqc/rma.py
 import streamlit as st
-import matplotlib as plt
-from wordcloud import WordCloud
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+#rma = pd.read_excel('https://raw.githubusercontent.com/noeke7236/rmaqc/main/2023/2023.xlsx')
+rma = pd.read_excel('/main/2023/2023.xlsx')
+rma_modified = rma.copy()
+
+#def load_data(url):
+#    return pd.read_excel(url)
+  
 st.set_page_config(
     page_title="RMA&QC",
     page_icon=":watermelon:",
@@ -19,6 +28,14 @@ def rma_qc():
 def rma_2023():
     st.markdown("# 2023 :tulip:")
     st.sidebar.markdown("# 2023 :tulip:")
+
+    row_count = len(rma_modified)
+    print("Total barang:",row_count)
+    rma_modified_qty = rma_modified['Qty'].sum()
+    print("Total kuantitas:", rma_modified_qty)
+    tabel_barang = [['Total Barang', row_count], ['Total Kuantitas', rma_modified_qty]]
+    st.subheader('Total barang yang masuk')
+    st.table(tabel_barang)
 
 #def page3():
 #    st.markdown("# Page 3 🎉")
