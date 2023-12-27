502537df6bc3b37a2dec297596d978b40e391241
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2023-12-27 12:33:14 +0700

    Update rma.py

--- rma.py
+++ rma.py
@@ -100,9 +100,8 @@ def rma_2023():
     
     # Mengganti nama kolom 'RMA Level' menjadi 'Level'
     tabel_persentase.index.name = 'Level'
-    #st.dataframe(tabel_persentase)
-    st.table(tabel_persentase.style.set_table_styles(styles).to_html())
-    #st.markdown(tabel_persentase.style.to_html(), unsafe_allow_html=True)
+    st.dataframe(tabel_persentase)
+    
         
 #def page3():
 #    st.markdown("# Page 3 🎉")
