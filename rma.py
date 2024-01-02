bf8eaf89f26fc564061636b8eceffc91be1209f0
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-01-02 11:21:01 +0700

    Update rma.py

--- rma.py
+++ rma.py
@@ -192,9 +192,7 @@ def rma_2023():
     # Menampilkan hasil di Streamlit
     st.text("")
     st.subheader('Daftar beberapa alat/barang yang Bad/Fail')
-    st.markdown(filtered_data_nok.style.to_html(), unsafe_allow_html=True)
-    #st.markdown(filtered_data_nok.style.hide(axis="index").to_html(), unsafe_allow_html=True)
-    #st.table(filtered_data_nok)
+    st.markdown(filtered_data_nok.style.hide(axis="index").to_html(), unsafe_allow_html=True)
     
     #GRAFIK BAR HORIZONTAL COUNT
     count_barang = rma_modified['Nama Barang'].value_counts().nlargest(10).sort_values(ascending=True)
