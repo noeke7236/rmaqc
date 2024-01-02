de1566eb817d5a61f4970f6d4d4f9296bd82f883
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-01-02 11:37:14 +0700

    Update rma.py

--- rma.py
+++ rma.py
@@ -36,6 +36,12 @@ def rma_qc():
     st.header('Alur Kerja', divider='rainbow')
     st.image('https://raw.githubusercontent.com/noeke7236/rmaqc/main/images/rma_flowchart.png')
     st.text("")
+    st.text("")
+    st.header('Knowledge Base', divider='rainbow')
+    url_link = "http://kb.mindotama.co.id/dokuwiki/doku.php?id=start"
+    st.write("RMA QC Knowledge Base [link](%s)" % url_link)
+    #st.markdown("check out this [link](%s)" % url)
+    st.image('https://raw.githubusercontent.com/noeke7236/rmaqc/main/images/kbmindotama.png')
  
 def rma_2023():
     st.markdown("# Statistik Tahun 2023")
