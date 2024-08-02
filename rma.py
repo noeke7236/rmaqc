d3685ca53d711f70cdf843ce54466510bb3f30ef
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-08-02 10:06:55 +0700

    Update rma.py

--- rma.py
+++ rma.py
@@ -200,6 +200,10 @@ url2 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT85fb9TAXVvoVOWoBQ2kRJ_
 rma2 = load_data(url2)
 rma_modified2 = rma2.copy()
 
+url3 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQPxyl1P5AOFTBbTNR2f1TH3jP69HJigz2nnixuT2Ft3E67jeQFerdFoD5heO9YSY-Zi_H7TjHrTu3x/pub?output=xlsx'
+rma3 = load_data(url3)
+#rma_modified3 = rma3.copy()
+
 #edit 01/08/2024
 # List standar nama kolom
 mylist = ['Nama Barang', 'Serial Number', 'Qty', 'RMA Level', 'Tgl Masuk [PB06]', 'Tgl Keluar [PB07]', 'Final Status', 'Project']
@@ -231,6 +235,18 @@ def rma_2022():
     st.markdown("# Infografis Tahun 2022")
     st.sidebar.markdown("# 2022 :bar_chart:")
 
+    #edit 02/08/2024
+    rma_modified3 = normalize_columns(rma3.copy(), columns_to_drop, mylist) #edit 01/08/2024
+    total_items2, total_quantity2 = calculate_statistics(rma_modified3)
+    rma2022_counts, rma2022_percentage, rma2022_pass_percentage, rma2022_fail_percentage = calculate_percentage(rma_modified3)
+
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Total Items", total_items2)
+    col2.metric("Total Quantity", total_quantity2)
+    col3.metric("Pass :heavy_check_mark:", f"{rma2022_pass_percentage:.1f}%")
+    col4.metric("Fail :x:", f"{rma2022_fail_percentage:.1f}%")
+    #edit 02/08/2024
+    
     st.markdown("""---""")
     
 def rma_2023():
