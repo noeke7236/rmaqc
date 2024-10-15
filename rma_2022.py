4efc660bda4828ec211ece4ac4b1cc92922763b4
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-10-15 15:13:28 +0700

    Update rma_2022.py

--- rma_2022.py
+++ rma_2022.py
@@ -7,6 +7,7 @@ from utils import load_data
 from utils import normalize_columns
 from utils import calculate_statistics
 from utils import calculate_percentage
+from utils import display_metrics
 
 url3 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQPxyl1P5AOFTBbTNR2f1TH3jP69HJigz2nnixuT2Ft3E67jeQFerdFoD5heO9YSY-Zi_H7TjHrTu3x/pub?output=xlsx'
 rma3 = load_data(url3)
