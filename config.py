d9e954f99855d3ac1f77ae5b09430e41fc8445f3
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-10-29 16:52:53 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+#config.py
+
+# List standar nama kolom
+mylist = ['Nama Barang', 'Serial Number', 'Qty', 'RMA Level', 'Tgl Masuk [PB06]', 'Tgl Keluar [PB07]', 'Final Status', 'Project']
+
+# Kolom yang harus dihapus untuk data tahun 2023
+columns_to_drop = ['Kategori','Expert','Tgl Tes','Tiket PB07']
