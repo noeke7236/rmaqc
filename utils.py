74d479ba9636175590f409fa9a84940db58dd86a
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2024-11-05 16:07:58 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ import seaborn as sns
 import streamlit as st
 import plotly.graph_objects as go
 
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
+
 # Month_names dictionary
 month_names = {
     '1': 'Januari',
@@ -127,7 +131,7 @@ def tabel_alat_barang(data):
     st.subheader('Total Alat / Barang')
     st.plotly_chart(fig)
 
-def grafik_barang(data, kolom_tanggal, kolom_jumlah, judul, warna):
+def statistik_barang(data, kolom_tanggal, judul, warna):
     data['Bulan'] = pd.to_datetime(data[kolom_tanggal], dayfirst=True).dt.strftime('%B')
     data['bulan'] = pd.to_datetime(data[kolom_tanggal], dayfirst=True).dt.month
     data['jumlah'] = 1
@@ -138,27 +142,52 @@ def grafik_barang(data, kolom_tanggal, kolom_jumlah, judul, warna):
     result_dataframe = pd.merge(data_bulan, result, on='bulan', how='left')
     result_dataframe['jumlah_y'] = result_dataframe['jumlah_y'].fillna(0).astype(int)
 
-    max_value = result_dataframe['jumlah_y'].max()
-    palette = ["#BEFF47" if y == max_value else warna for y in result_dataframe['jumlah_y']]
+    return result_dataframe
 
-    fig_bar, ax_bar = plt.subplots(figsize=(12, 6))
-    #04102024
-    #bar_plot = sns.barplot(data=result_dataframe, x="bulan", y="jumlah_y", palette=palette)
-    bar_plot = sns.barplot(data=result_dataframe, x="bulan", y="jumlah_y", hue="bulan", palette=palette, ax=ax_bar, legend=False)
-    #04102024
-    ax_bar.set_xlabel('Bulan')
-    ax_bar.set_ylabel('Jumlah barang')
-    ax_bar.set_ylim(0, max_value + 50)
+def grafik_barang(data_masuk, data_keluar):
+    bar = (
+        Bar()
+        .add_xaxis([month_names[str(i)] for i in range(1, 13)])
+        .add_yaxis("Barang Masuk", data_masuk['jumlah_y'].tolist(), color="#009EFA")
+        .add_yaxis("Barang Keluar", data_keluar['jumlah_y'].tolist(), color="#FF6347")
+        .set_global_opts(
+            #title_opts=opts.TitleOpts(title=judul),
+            xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=-45)),
+        )
+    )
+    return bar
 
-    for p in ax_bar.patches:
-        height = p.get_height()
-        ax_bar.annotate(f'{height:.0f}', (p.get_x() + p.get_width() / 2., height), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+#def grafik_barang(data, kolom_tanggal, kolom_jumlah, judul, warna):
+    #data['Bulan'] = pd.to_datetime(data[kolom_tanggal], dayfirst=True).dt.strftime('%B')
+    #data['bulan'] = pd.to_datetime(data[kolom_tanggal], dayfirst=True).dt.month
+    #data['jumlah'] = 1
+    #result = data.groupby(['bulan']).agg(jumlah=('jumlah', 'count')).reset_index()
 
-    ax_bar.set_xticks(range(len(month_names)))
-    ax_bar.set_xticklabels([month_names[str(i)] for i in range(1, 13)], rotation=20)
-    st.text("")
-    st.subheader(judul)
-    st.pyplot(fig_bar)
+    #no_bulan = [{'bulan': i, 'jumlah': 0} for i in range(1, 13)]
+    #data_bulan = pd.DataFrame(no_bulan)
+    #result_dataframe = pd.merge(data_bulan, result, on='bulan', how='left')
+    #result_dataframe['jumlah_y'] = result_dataframe['jumlah_y'].fillna(0).astype(int)
+
+    #max_value = result_dataframe['jumlah_y'].max()
+    #palette = ["#BEFF47" if y == max_value else warna for y in result_dataframe['jumlah_y']]
+
+    #fig_bar, ax_bar = plt.subplots(figsize=(12, 6))
+    # Tanggal 04102024
+    #bar_plot = sns.barplot(data=result_dataframe, x="bulan", y="jumlah_y", hue="bulan", palette=palette, ax=ax_bar, legend=False)
+    # Tanggal 04102024
+    #ax_bar.set_xlabel('Bulan')
+    #ax_bar.set_ylabel('Jumlah barang')
+    #ax_bar.set_ylim(0, max_value + 50)
+
+    #for p in ax_bar.patches:
+        #height = p.get_height()
+        #ax_bar.annotate(f'{height:.0f}', (p.get_x() + p.get_width() / 2., height), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+
+    #ax_bar.set_xticks(range(len(month_names)))
+    #ax_bar.set_xticklabels([month_names[str(i)] for i in range(1, 13)], rotation=20)
+    #st.text("")
+    #st.subheader(judul)
+    #st.pyplot(fig_bar)
 
 def total_barang_masuk(data):
     row_count, total_qty = calculate_statistics(data)
