1c44038b0aea72344bf13c3ebd91e246a6770af5
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2025-02-13 10:02:55 +0700

    Update rma_2022.py

--- rma_2022.py
+++ rma_2022.py
@@ -8,7 +8,7 @@ from streamlit_echarts import st_pyecharts
 from config import tahun_2022
 from config import mylist
 
-from utils import load_data
+from utils import load_data_new
 from utils import normalize_columns
 from utils import calculate_statistics
 from utils import calculate_percentage
@@ -18,13 +18,11 @@ from utils import tampilkan_pie_chart
 from utils import statistik_barang
 from utils import grafik_barang
 
-#url3 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQPxyl1P5AOFTBbTNR2f1TH3jP69HJigz2nnixuT2Ft3E67jeQFerdFoD5heO9YSY-Zi_H7TjHrTu3x/pub?output=xlsx'
-#rma3 = load_data(url3)
-
 #mylist = ['Nama Barang', 'Serial Number', 'Qty', 'RMA Level', 'Tgl Masuk [PB06]', 'Tgl Keluar [PB07]', 'Final Status', 'Project']
 
 def rma_2022():
-    rma = load_data(tahun_2022['url'])
+    #rma = load_data(tahun_2022['url'])
+    rma = load_data_new(tahun_2022['tipe'], tahun_2022['url'])
     rma_modified = rma.copy()
     rma_modified = normalize_columns(rma_modified, mylist)
 
