f05675dbe4cfae51bbf96b688ec8148ad9de8235
Ponco Nugrah Wibowo <noeke7236@gmail.com>
2025-10-24 15:01:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 # Dictionary
 tahun_2022 = {'tahun' : '2022', 'tipe' : 'excel', 'url' : 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQPxyl1P5AOFTBbTNR2f1TH3jP69HJigz2nnixuT2Ft3E67jeQFerdFoD5heO9YSY-Zi_H7TjHrTu3x/pub?output=xlsx', 'icon' : ':tiger:', 'icon_sidebar' : ':tiger2:'}
-#tahun_2023 = {'tahun' : '2023', 'tipe' : 'excel', 'url' : 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2gUwmQqoZnuheu3yON7gG9yep2apr1Hwcs9xvb4Ce1yxkIBNAHZmDoarWHOUymQ/pub?output=xlsx', 'icon' : ':rabbit:', 'icon_sidebar' : ':rabbit2:'}
-tahun_2023 = {'tahun' : '2023', 'tipe' : 'excel', 'url' : '/2023/2023all.xlsx', 'icon' : ':rabbit:', 'icon_sidebar' : ':rabbit2:'}
+tahun_2023 = {'tahun' : '2023', 'tipe' : 'excel', 'url' : 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2gUwmQqoZnuheu3yON7gG9yep2apr1Hwcs9xvb4Ce1yxkIBNAHZmDoarWHOUymQ/pub?output=xlsx', 'icon' : ':rabbit:', 'icon_sidebar' : ':rabbit2:'}
+#tahun_2023 = {'tahun' : '2023', 'tipe' : 'excel', 'url' : '/2023/2023all.xlsx', 'icon' : ':rabbit:', 'icon_sidebar' : ':rabbit2:'}
 tahun_2024 = {'tahun' : '2024', 'tipe' : 'excel', 'url' : 'https://docs.google.com/spreadsheets/d/14ZlFWCz-zuubj1L6XAHa_E9IH1Yvbb-B/export?format=xlsx&id=14ZlFWCz-zuubj1L6XAHa_E9IH1Yvbb-B', 'icon' : ':dragon_face:', 'icon_sidebar' : ':dragon:'}
 #tahun_2024 = {'tahun' : '2024', 'tipe' : 'excel', 'url' : '/2024/2024all.xlsx', 'icon' : ':dragon_face:', 'icon_sidebar' : ':dragon:'}
 #tahun_2025 = {'tahun' : '2025', 'tipe' : 'csv', 'url' : 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSrI-S-HxETvnbQKJmbldzFj8G6cOTddrvGgtDtftsiJRd7vz2RApDPFXdx_TkU0xZz04Zp1Yvh146f/pub?output=csv', 'icon' : ' ', 'icon_sidebar' : ':snake:'}
